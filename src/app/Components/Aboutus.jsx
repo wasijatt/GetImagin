f8a195860d2df077ab48f178ffc3d2063e79647c
Muhammad Wasim <muhammmadwasim5686@gmail.com>
2024-11-10 02:47:35 -0800

    some ui changes

--- src/app/Components/Aboutus.jsx
+++ src/app/Components/Aboutus.jsx
@@ -1,3 +1,4 @@
+"use client"
 import dynamic from 'next/dynamic';
 import { FaArrowLeft } from "react-icons/fa";
 import Image from "next/image";
--- src/app/Components/Login.jsx
+++ src/app/Components/Login.jsx
@@ -1,9 +1,9 @@
 
 
 "use client";
-import Link from "next/link";
+
 import { useState } from "react";
-import AnimatedLink from "./AnimatedLink";
+
 
 const Login = () => {
     const [selectedItems, setSelectedItems] = useState([]);
